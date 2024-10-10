8c693730dc16150064870493afdaa0ad9f00e2de
carlos14500 <carlos14500@outlook.es>
2024-10-10 17:03:11 -0600

    Set the formatted date

--- app.py
+++ app.py
@@ -1,32 +1,77 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import pyodbc
+from flask import Flask, redirect, render_template, request, url_for, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+def get_db_connection():
+    try:
+        print("Trying to connect to database...")
+        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=CARLOSEMILIO;DATABASE=Mediciones;UID=sa;PWD=root')
+        print("Connection succeded")
+        return connection
+    except Exception as ex:
+        print(f"Error to reach connection: {ex}")
+        return None
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/get_data', methods=['POST'])
+def get_data():
+    client_request = request.get_json()
+    date = client_request.get('date')
 
+    connection = get_db_connection()
+
+    if connection:
+        try:
+            print(client_request)
+            cursor = connection.cursor()
+            query = ""
+            if date not in [None, "", []]:
+                formatted_date = datetime.strptime(date,"%Y-%m-%d")
+                query = f"SELECT ID, PM_1, PM2_5, PM_10 FROM M_UPIITA WHERE Fecha = CONVERT(datetime,'{date}')" 
+            else:
+                query = "SELECT ID, Fecha, PM_1, PM2_5, PM_10 FROM M_UPIITA WHERE CAST(Fecha AS DATE) = (SELECT MAX(CAST(Fecha AS DATE))FROM M_UPIITA)"
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            
+            pm1 = 0
+            pm2 = 0
+            pm10 = 0
+
+            for row in rows:
+                pm1 = pm1 + row[2]
+                pm2 = pm2 + row[3]
+                pm10 = pm10 + row[4]
+
+            pm1_average = pm1/len(rows)
+            pm2_average = pm2/len(rows)
+            pm10_average = pm10/len(rows)
+            
+            data = {
+                'DATE' : row[1],
+                'PM1': pm1_average,
+                'PM2.5': pm2_average,
+                'PM10': pm10_average
+            }
+
+            return data
+
+        except Exception as ex:
+            print(f"Error to execute SQL Query: {ex}")
+            return jsonify({'error': f'Error to execute SQL Query: {ex}'})
+        
+        finally:
+            connection.close()
+            print("Connection to DB closed.")
+    
+    else:
+        print("Can't connect to DB.")
+        return jsonify({'error': 'Can\'t connect to DB.'})
 
 if __name__ == '__main__':
-   app.run()
+    print("Servidor Flask ...")
+    app.run(debug=True)
