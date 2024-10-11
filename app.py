94f6966de230b8d7418ae763f855999d2022276a
carlos14500 <carlos14500@outlook.es>
2024-10-11 17:36:19 -0600

    Added multiple table querys

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import pyodbc
 from flask import Flask, redirect, render_template, request, url_for, jsonify
 from datetime import datetime
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -9,7 +10,14 @@ app = Flask(__name__)
 def get_db_connection():
     try:
         print("Trying to connect to database...")
-        connection = pyodbc.connect('DRIVER={SQL Server};SERVER=CARLOSEMILIO;DATABASE=Mediciones;UID=sa;PWD=root')
+        load_dotenv()
+        database = os.environ.get('DATABASE')
+        server = os.environ.get('SERVER')
+        uid = os.environ.get('UID')
+        pwd = os.environ.get('PWD')
+        print(database,server,uid,pwd)
+        
+        connection = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}')
         print("Connection succeded")
         return connection
     except Exception as ex:
@@ -18,8 +26,8 @@ def get_db_connection():
 
 
 
-@app.route('/get_data', methods=['POST'])
-def get_data():
+@app.route('/get_indicators_data', methods=['POST'])
+def get_indicators_data():
     client_request = request.get_json()
     date = client_request.get('date')
 
@@ -27,38 +35,45 @@ def get_data():
 
     if connection:
         try:
-            print(client_request)
-            cursor = connection.cursor()
-            query = ""
-            if date not in [None, "", []]:
-                formatted_date = datetime.strptime(date,"%Y-%m-%d")
-                query = f"SELECT ID, PM_1, PM2_5, PM_10 FROM M_UPIITA WHERE Fecha = CONVERT(datetime,'{date}')" 
-            else:
-                query = "SELECT ID, Fecha, PM_1, PM2_5, PM_10 FROM M_UPIITA WHERE CAST(Fecha AS DATE) = (SELECT MAX(CAST(Fecha AS DATE))FROM M_UPIITA)"
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            
-            pm1 = 0
-            pm2 = 0
-            pm10 = 0
+            indicators = []
+            for table in ['M_UPIITA', 'M_ESCOM', 'M_CDA']:
+                print(client_request)
+                cursor = connection.cursor()
+                query = ""
+                if date not in [None, "", []]:
+                    formatted_date = datetime.strptime(date,"%Y-%m-%d")
+                    query = f"SELECT ID, Fecha, PM_1, PM2_5, PM_10 FROM {table} WHERE Fecha = CONVERT(datetime,'{date}')" 
+                else:
+                    query = f"SELECT ID, Fecha, PM_1, PM2_5, PM_10 FROM {table} WHERE CAST(Fecha AS DATE) = (SELECT MAX(CAST(Fecha AS DATE))FROM M_UPIITA)"
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                
+                if not rows:
+                    return []
 
-            for row in rows:
-                pm1 = pm1 + row[2]
-                pm2 = pm2 + row[3]
-                pm10 = pm10 + row[4]
+                pm1 = 0
+                pm2 = 0
+                pm10 = 0
 
-            pm1_average = pm1/len(rows)
-            pm2_average = pm2/len(rows)
-            pm10_average = pm10/len(rows)
-            
-            data = {
-                'DATE' : row[1],
-                'PM1': pm1_average,
-                'PM2.5': pm2_average,
-                'PM10': pm10_average
-            }
+                for row in rows:
+                    pm1 = pm1 + row[2]
+                    pm2 = pm2 + row[3]
+                    pm10 = pm10 + row[4]
 
-            return data
+                pm1_average = pm1/len(rows)
+                pm2_average = pm2/len(rows)
+                pm10_average = pm10/len(rows)
+                
+                data = {
+                    'SOURCE': table,
+                    'DATE' : row[1],
+                    'PM1': pm1_average,
+                    'PM2.5': pm2_average,
+                    'PM10': pm10_average
+                }
+                
+                indicators.append(data)
+            return indicators
 
         except Exception as ex:
             print(f"Error to execute SQL Query: {ex}")
